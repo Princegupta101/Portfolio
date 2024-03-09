dfe91c41f10f6d8d462257ead71636bd1988846b
Princegupta101 <pgupta28349@gmail.com>
2024-03-09 16:47:08 +0530

    footer Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/about"
 import Contact from "@/components/contact"
 import Experience from "@/components/experience"
+import Footer from "@/components/footer"
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider"
@@ -16,6 +17,7 @@ export default function Home() {
       <Skills/>
       <Experience/>
       <Contact/>
+      <Footer/>
     </main>
   );
 }
